092327e31d63c52bc1bab7323a03fb7327d13ac6
Mick van Gelderen <mickvangelderen@gmail.com>
2019-07-21 08:58:43 +0200

    Great its doing what I want

--- src/bin/asdf.rs
+++ src/bin/asdf.rs
@@ -4,63 +4,84 @@ use std::collections::HashMap;
 use std::path::{Path, PathBuf};
 use std::rc::Rc;
 
-type FileIndex = usize;
+type SourceIndex = usize;
 
 #[derive(Debug, Clone)]
 enum Token {
     Literal(String),
-    Include(SourcePath),
+    Include(PathBuf),
 }
 
-type File = Vec<Token>;
+type Tokens = Vec<Token>;
 
 #[derive(Debug)]
 struct Disk {
-    files: HashMap<PathBuf, File>,
+    files: HashMap<PathBuf, Tokens>,
 }
 
 impl Disk {
-    fn read(&self, path: impl AsRef<Path>) -> File {
+    fn read(&self, path: impl AsRef<Path>) -> Tokens {
         self.files.get(path.as_ref()).cloned().expect("No such file.")
     }
 }
 
+#[derive(Debug)]
+struct Variables {
+    attenuation_mode: u32,
+    render_technique: u32,
+}
+
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
-enum SourcePath {
+enum SourceReader {
     File(PathBuf),
     AttenuationMode,
     RenderTechnique,
 }
 
-#[derive(Debug)]
-struct CachedFile {
-    path: SourcePath,
-    last_modified: Revision,
-    last_computed: Revision,
-    tokens: File,
+impl SourceReader {
+    fn read(&self, tokens: &mut Tokens, disk: &Disk, vars: &Variables) {
+        match *self {
+            SourceReader::File(ref path) => {
+                *tokens = disk.read(path);
+            },
+            SourceReader::AttenuationMode => {
+                *tokens = vec![
+                    Token::Literal(format!("#define ATTENUATION_MODE {}\n", vars.attenuation_mode)),
+                ];
+            }
+            SourceReader::RenderTechnique => {
+                *tokens = vec![
+                    Token::Literal(format!("#define RENDER_TECHNIQUE {}\n", vars.render_technique)),
+                ];
+            }
+        }
+    }
 }
 
-impl CachedFile {
-    fn update(&mut self, disk: &Disk) {
+#[derive(Debug)]
+struct Source {
+    reader: SourceReader,
+    last_modified: Revision,
+    last_computed: Revision,
+    tokens: Tokens,
+}
+
+impl Source {
+    fn update(&mut self, disk: &Disk, vars: &Variables) {
         if self.last_computed == self.last_modified {
             println!("No need to update {:?}, file has not been modified.", self);
-            // File is up-to-date.
+            // Tokens is up-to-date.
         } else {
             debug_assert!(self.last_computed < self.last_modified);
             self.last_computed = self.last_modified;
 
-            // Read file from disk.
-            self.tokens = match self.path {
-                SourcePath::File(ref path) => disk.read(path),
-                SourcePath::AttenuationMode => vec![Token::Literal(format!("{}:{}\n", file!(), line!()))],
-                SourcePath::RenderTechnique => vec![Token::Literal(format!("{}:{}\n", file!(), line!()))],
-            };
+            self.reader.read(&mut self.tokens, disk, vars);
 
             println!("Updated {:?}.", self);
         }
     }
 
-    fn tokens(&self, parent: &mut Revision) -> &File {
+    fn tokens(&self, parent: &mut Revision) -> &Tokens {
         assert_eq!(self.last_computed, self.last_modified);
 
         if *parent < self.last_computed {
@@ -73,23 +94,24 @@ impl CachedFile {
 
 #[derive(Debug)]
 struct Memory {
-    path_to_file_index: HashMap<SourcePath, FileIndex>,
-    files: Vec<Rc<CachedFile>>,
+    path_to_file_index: HashMap<PathBuf, SourceIndex>,
+    files: Vec<Rc<Source>>,
 }
 
 impl Memory {
-    fn file_index(&mut self, global: &Global, path: &SourcePath) -> FileIndex {
+    fn file_index(&mut self, global: &Global, path: impl AsRef<Path>) -> SourceIndex {
+        let path = path.as_ref();
         match self.path_to_file_index.get(path) {
             Some(&file_index) => file_index,
             None => {
                 let file_index = self.files.len();
-                self.files.push(Rc::new(CachedFile {
-                    path: path.clone(),
+                self.files.push(Rc::new(Source {
+                    reader: SourceReader::File(PathBuf::from(path)),
                     last_modified: global.revision,
                     last_computed: Revision::DIRTY,
                     tokens: Vec::new(),
                 }));
-                self.path_to_file_index.insert(path.clone(), file_index);
+                self.path_to_file_index.insert(PathBuf::from(path), file_index);
                 file_index
             }
         }
@@ -98,11 +120,11 @@ impl Memory {
 
 #[derive(Debug)]
 struct EntryPoint {
-    file_index: FileIndex,
+    file_index: SourceIndex,
     last_verified: Revision,
     last_computed: Revision,
     contents: String,
-    included: Vec<FileIndex>,
+    included: Vec<SourceIndex>,
 }
 
 enum Presence {
@@ -120,7 +142,7 @@ fn vec_set_add<T: Copy + PartialEq>(vec: &mut Vec<T>, val: T) -> Presence {
 }
 
 impl EntryPoint {
-    fn update(&mut self, global: &Global, mem: &mut Memory, disk: &Disk) {
+    fn update(&mut self, global: &Global, mem: &mut Memory, disk: &Disk, vars: &Variables) {
         if self.last_verified == global.revision {
             println!("No need to update {:?}, already verified.", self);
             return;
@@ -147,16 +169,16 @@ impl EntryPoint {
         self.contents.clear();
         self.included.clear();
 
-        process(self, global, mem, disk, self.file_index);
+        process(self, global, mem, disk, vars, self.file_index);
 
-        fn process(ep: &mut EntryPoint, global: &Global, mem: &mut Memory, disk: &Disk, file_index: FileIndex) {
+        fn process(ep: &mut EntryPoint, global: &Global, mem: &mut Memory, disk: &Disk, vars: &Variables, file_index: SourceIndex) {
             // Stop processing if we've already included this file.
             if let Presence::Duplicate = vec_set_add(&mut ep.included, file_index) {
                 return;
             }
 
             let file = Rc::get_mut(&mut mem.files[file_index]).unwrap();
-            file.update(disk);
+            file.update(disk, vars);
 
             // Clone the file rc so we can access tokens while mutating the tokens vec.
             let file = Rc::clone(&mem.files[file_index]);
@@ -168,7 +190,7 @@ impl EntryPoint {
                     }
                     Token::Include(ref path) => {
                         let file_index = mem.file_index(global, path);
-                        process(ep, global, mem, disk, file_index);
+                        process(ep, global, mem, disk, vars, file_index);
                     }
                 }
             }
@@ -179,16 +201,19 @@ impl EntryPoint {
 }
 
 fn main() {
+    let attenuation_mode_path = PathBuf::from("native/ATTENUATION_MODE.glsl");
+    let render_technique_path = PathBuf::from("native/RENDER_TECHNIQUE.glsl");
+
     let mut disk = Disk {
         files: vec![
             (
                 PathBuf::from("a.txt"),
                 vec![
-                    Token::Include(SourcePath::AttenuationMode),
+                    Token::Include(attenuation_mode_path.clone()),
                     Token::Literal("a.txt:1\n".to_string()),
-                    Token::Include(SourcePath::File(PathBuf::from("b.txt"))),
+                    Token::Include(PathBuf::from("b.txt")),
                     Token::Literal("a.txt:3\n".to_string()),
-                    Token::Include(SourcePath::File(PathBuf::from("c.txt"))),
+                    Token::Include(PathBuf::from("c.txt")),
                     Token::Literal("a.txt:5\n".to_string()),
                 ],
             ),
@@ -196,13 +221,13 @@ fn main() {
                 PathBuf::from("b.txt"),
                 vec![
                     Token::Literal("b.txt:1\nb.txt:2\n".to_string()),
-                    Token::Include(SourcePath::File(PathBuf::from("c.txt"))),
+                    Token::Include(PathBuf::from("c.txt")),
                 ],
             ),
             (
                 PathBuf::from("c.txt"),
                 vec![
-                    Token::Include(SourcePath::File(PathBuf::from("a.txt"))),
+                    Token::Include(PathBuf::from("a.txt")),
                     Token::Literal("c.txt:1\nc.txt:2\n".to_string()),
                 ],
             ),
@@ -218,8 +243,31 @@ fn main() {
         files: Vec::new(),
     };
 
+    let vars = Variables {
+        attenuation_mode: 1,
+        render_technique: 6,
+    };
+
+    let attenuation_mode_index = mem.files.len();
+    mem.files.push(Rc::new(Source {
+        reader: SourceReader::AttenuationMode,
+        last_modified: global.revision,
+        last_computed: Revision::DIRTY,
+        tokens: Vec::new(),
+    }));
+    mem.path_to_file_index.insert(attenuation_mode_path, attenuation_mode_index);
+
+    let render_technique_index = mem.files.len();
+    mem.files.push(Rc::new(Source {
+        reader: SourceReader::RenderTechnique,
+        last_modified: global.revision,
+        last_computed: Revision::DIRTY,
+        tokens: Vec::new(),
+    }));
+    mem.path_to_file_index.insert(render_technique_path, render_technique_index);
+
     let mut entry = {
-        let file_index = mem.file_index(&global, &SourcePath::File(PathBuf::from("a.txt")));
+        let file_index = mem.file_index(&global, "a.txt");
         EntryPoint {
             file_index,
             last_verified: Revision::DIRTY,
@@ -229,10 +277,10 @@ fn main() {
         }
     };
 
-    entry.update(&global, &mut mem, &disk);
-    entry.update(&global, &mut mem, &disk);
+    entry.update(&global, &mut mem, &disk, &vars);
+    entry.update(&global, &mut mem, &disk, &vars);
     global.revision.0 += 1;
-    entry.update(&global, &mut mem, &disk);
+    entry.update(&global, &mut mem, &disk, &vars);
 
     println!("{}", &entry.contents);
 
@@ -244,14 +292,14 @@ fn main() {
         disk.files.get_mut(Path::new("a.txt")).unwrap(),
         vec![
             Token::Literal("a.txt:1\n".to_string()),
-            Token::Include(SourcePath::File(PathBuf::from("d.txt"))),
+            Token::Include(PathBuf::from("d.txt")),
         ],
     );
 
     global.revision.0 += 1;
     Rc::get_mut(&mut mem.files[entry.file_index]).unwrap().last_modified = global.revision;
 
-    entry.update(&global, &mut mem, &disk);
+    entry.update(&global, &mut mem, &disk, &vars);
 
     println!("{}", &entry.contents);
 }
